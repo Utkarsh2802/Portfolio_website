f84231c81b6b76097a0ed403c34f8a47df245cde
Utkarsh2802 <utkarshagarwal2802@yahoo.com>
2021-07-10 03:11:24 +0530

    Completed the homepage design made it completely responsive with media queries

--- utkarsh_frontend/src/Pages/HomePage.js
+++ utkarsh_frontend/src/Pages/HomePage.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "../Design/Own.css";
+//import "../Design/Own.css";
 import PlaySound from "../Components/PlaySound";
 import Tones from "../Data/Songs/TONES AND I - Dance Monkey.mp3";
 import useWindowDimensions from "../Utility_functions/UseWIndowDimensions";
+import "../Design/HomePage.css";
 const HomePage = () => {
   document.body.style.padding = 0;
   document.body.style.margin = 0;
@@ -13,7 +14,38 @@ const HomePage = () => {
         <source src={sample} type="video/mp4" />
       </video>
     */
-  return <div style={{ height: height, width: width }}></div>;
+  return (
+    <div
+      style={{
+        height: height,
+        width: width,
+        display: "flex",
+        backgroundColor: "#e7e5e4",
+        flexDirection: "column",
+
+        gap: "13vh",
+        alignItems: "center",
+      }}
+    >
+      <h1 className="shadowytext">Typing God</h1>
+      <div className="containerofoption">
+        <div className="heading">Wanna become a Typing God?</div>
+        <div className="arrow "></div>
+
+        <div className="heading">
+          <button className="button">Singup</button> OR{" "}
+          <button className="button">Login</button>
+        </div>
+      </div>
+      <div className="containerofoption">
+        <div className="heading">Just Wanna test your Typing Speed?</div>
+        <div className="arrow"></div>
+        <div className="heading">
+          <button className="button">Typing Test</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
