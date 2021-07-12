cec7a0f4b8563141247c7b6307f9f0240958bec6
Utkarsh2802 <utkarshagarwal2802@yahoo.com>
2021-07-12 21:43:06 +0530

    added image onload fixed music icon not visible issue on the typingtest page

--- utkarsh_frontend/src/Pages/TypingSpeedTest.js
+++ utkarsh_frontend/src/Pages/TypingSpeedTest.js
@@ -2,41 +2,54 @@ import React, { useState, useEffect } from "react";
 
 import "../Design/Own.css";
 import TypingHelper from "../Helpers/TypingHelper";
-import pexel2 from "../Data/Images/pexels9.jpg"; //confused between either 3 or 2
+import pexels9 from "../Data/Images/pexels9.jpg"; //confused between either 3 or 2
 import { Animated } from "react-animated-css";
 import useWindowDimensions from "../Utility_functions/UseWIndowDimensions";
 const TypingSpeedTest = () => {
-  console.log("helo");
-  const [image, setImage] = useState("");
+  // Uncomment everything to use the img tag instead of background image all this code helps in resizing the image depending on the window size
+  // const [image, setImage] = useState("");
   //document.body.style ="background-image: linear-gradient(90deg, #0093E9 0%, #80D0C7 100%)";
-  document.body.style.padding = 0;
-  document.body.style.margin = 0;
+  // document.body.style.padding = 0;
+  // document.body.style.margin = 0;
   document.body.style.overflowY = "hidden";
-  //very important to cover the whole page
-  // document.body.style = "background-image:" + homepageimg;
-  //    <img src={homepageig} height="100vw" />
-  const { height, width } = useWindowDimensions(); //this gives mean the exact height whenever i resize my
+  // const { height, width } = useWindowDimensions(); //this gives mean the exact height whenever i resize my
+  // useEffect(() => {
+  //   setImage(
+  //     <img
+  //       src={pexel2}
+  //       height={height}
+  //       width={width}
+  //       alt="Some Error occured while loading the background"
+  //     />
+  //   );
+  // }, [height, width]);
+  const [loaded, setLoaded] = useState(false);
+  const img = new Image();
+  img.onload = () => {
+    console.log("loaded");
+    setLoaded(true);
+  };
+  img.src = pexels9;
   useEffect(() => {
-    setImage(
-      <img
-        src={pexel2}
-        height={height}
-        width={width}
-        alt="Some Error occured while loading the background"
-      />
-    );
-  }, [height, width]);
+    console.log("hi utk");
+  }, []);
   return (
     <div>
-      <Animated
-        animationIn="fadeIn"
-        animationInDelay={500}
-        animationInDuration={1000}
-        animateOnMount={true}
-        isVisible={true}
-      >
-        {image}
-      </Animated>
+      {loaded ? (
+        <div
+          style={{
+            height: "100vh",
+            width: "100vw",
+            backgroundImage: `url(${pexels9})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+      ) : (
+        ""
+      )}
+
       <div style={{ position: "absolute", top: "5vh", left: 0 }}>
         <TypingHelper></TypingHelper>
       </div>
