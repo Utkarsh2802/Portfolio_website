3b10751d6ae88f455f1525ff2e79754b950d4b25
Utkarsh2802 <utkarshagarwal2802@yahoo.com>
2022-04-24 00:28:41 +0530

    changed backend link

--- utkarsh_frontend/src/Apis/HandleApi.js
+++ utkarsh_frontend/src/Apis/HandleApi.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 axios.defaults.withCredentials = true; //to allow cookies to be send with the request
 const api = axios.create({
-  baseURL: "https://typinggodbackend.herokuapp.com", //"http://127.0.0.1:5000",
+  baseURL: "https://playful-cassata-989d10.netlify.app", //"http://127.0.0.1:5000",
   //timeout: 10,
 });
 const HandleApi = (type, url, data) => {
